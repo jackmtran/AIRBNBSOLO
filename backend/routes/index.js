f288ba928f6916ade308f7111ff5453bfb85dbe2
Jack <tranmjack@gmail.com>
2022-06-23 10:28:57 -0700

    fix

---
+++ backend/routes/index.js
@@ -0,0 +1,17 @@
+// backend/routes/index.js
+const express = require('express');
+const router = express.Router();
+
+router.get('/hello/world', function(req, res) {
+  res.cookie('XSRF-TOKEN', req.csrfToken());
+  res.send('Hello World!');
+});
+
+// backend/routes/index.js
+// ...
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
+// ...
+
+module.exports = router;
