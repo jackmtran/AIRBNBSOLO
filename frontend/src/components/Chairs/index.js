d0b41822258a1e7941fc04fe7da9996f957a1b59
Jack <tranmjack@gmail.com>
2022-06-28 23:35:23 -0700

    morestyling LOGO and BUTTONS

--- frontend/src/components/Chairs/index.js
+++ frontend/src/components/Chairs/index.js
@@ -1,33 +1,45 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getChairs } from '../../store/chairs';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useHistory } from 'react-router-dom';
 import './Chairs.css'
 
 function ShowChairs() {
 
     const dispatch = useDispatch();
+    const history = useHistory();
 
     const chairsObject = useSelector(state => state.chairs);
     const chairsArray = Object.values(chairsObject);
 
-    useEffect (()=> {
+    useEffect(() => {
         dispatch(getChairs())
     }, [dispatch])
 
     return (
         <>
-        <h1>All Chairs</h1>
-        {chairsObject && chairsArray.map(chair => {
-            return <ul className='gridthechairs' key={chair.id}>
-                <ul>{chair.name}</ul>
-                <ul>{chair.description}</ul>
-                <ul>${chair.price}/day</ul>
-                <ul>{chair.address}, { chair.city}, { chair.state}</ul>
-                <ul><button className='button'><NavLink className='navlinkEDIT' to={`/chair/${chair.id}`}>Edit</NavLink></button></ul>
-                <ul><button className='button'><NavLink className='navlinkEDIT' to={`/chair/${chair.id}`}>Add A Review</NavLink></button></ul>
-            </ul>
-        })}
+            <h1>Hey, have a seat.</h1>
+            {chairsObject && chairsArray.map(chair => {
+                // if(chair.userId === userId) reveal edit button
+
+                const forHoverEDIT = (e) => {
+                    e.preventDefault();
+                    history.push(`/chair/${chair.id}`)
+                }
+                const forHoverADDREV = (e) => {
+                    e.preventDefault();
+                    history.push(`/chair/${chair.id}`)
+                }
+
+                return <ul className='gridthechairs' key={chair.id}>
+                    <ul>{chair.name}</ul>
+                    <ul>{chair.description}</ul>
+                    <ul>${chair.price}/day</ul>
+                    <ul>{chair.address}, {chair.city}, {chair.state}</ul>
+                    <ul><button className='button buttonspace' onClick={forHoverEDIT}>Edit</button>
+                    <button className='button buttonspace' onClick={forHoverADDREV}>Add A Review</button></ul>
+                </ul>
+            })}
         </>
     )
 }
